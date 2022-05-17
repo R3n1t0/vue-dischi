9d01c4b9954b3ff67da8115e9df078a637922ae6
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-17 22:27:13 +0200

    componenti e immagine

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
+  <div class="container">
+
+    <HeaderComp />
+    <CardsComp />
   
   </div>
 </template>
 
 <script>
+import CardsComp from './components/CardsComp.vue';
+import HeaderComp from './components/HeaderComp.vue';
 
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    CardsComp,
+    HeaderComp
+}
 }
 </script>
 
---
+++ src/components/CardsComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>main</div>
+</template>
+
+<script>
+export default {
+  name: "CardsComp"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
